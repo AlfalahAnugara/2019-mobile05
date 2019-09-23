1fba8db6a53524a2d7ef321cf33583d4b8e7c392
AlfalahAnugara <Alfalah190299@gmail.com>
2019-09-23 15:06:15 +0700

    Proses pengerjaan praktikum 05

---
+++ app/src/main/java/polinema/ac/id/starterchapter05/activities/DynamicActivity.java
@@ -0,0 +1,15 @@
+package polinema.ac.id.starterchapter05.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import polinema.ac.id.starterchapter05.R;
+
+import android.os.Bundle;
+
+public class DynamicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dynamic);
+    }
+}
