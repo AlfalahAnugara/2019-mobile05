6af61b23a484739ab8931a52a5e0ac2a64a31b13
AlfalahAnugara <Alfalah190299@gmail.com>
2019-09-23 17:10:44 +0700

    Proses pengerjaan praktikum 05

--- app/src/main/java/polinema/ac/id/starterchapter05/activities/DynamicActivity.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/activities/DynamicActivity.java
@@ -3,6 +3,7 @@ package polinema.ac.id.starterchapter05.activities;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 import polinema.ac.id.starterchapter05.R;
+import polinema.ac.id.starterchapter05.fragments.BlueFragment;
 import polinema.ac.id.starterchapter05.fragments.RedFragment;
 
 import android.os.Bundle;
@@ -16,15 +17,30 @@ public class DynamicActivity extends AppCompatActivity {
         setContentView(R.layout.activity_dynamic);
     }
 
-    public void handleClickLoadRedFragment(View view) {
+//    public void handleClickLoadRedFragment(View view) {
 //        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
 //        fragmentTransaction.replace(R.id.dynamic_fragment_placeholder,new RedFragment());
 //        fragmentTransaction.commit();
+//    }
+//
+//    public void handleClickLoadBlueFragment(View view) {
+//        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+//        fragmentTransaction.replace(R.id.dynamic_fragment_placeholder,new BlueFragment());
+//        fragmentTransaction.commit();
+//    }
+
+    public void handleClickLoadRedFragment(View view) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.dynamic_fragment_placeholder,new RedFragment(),"RED_FRAGMENT");
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 
     public void handleClickLoadBlueFragment(View view) {
-//        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-//        fragmentTransaction.replace(R.id.dynamic_fragment_placeholder,new RedFragment());
-//        fragmentTransaction.commit();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.dynamic_fragment_placeholder,new BlueFragment(),"BLUE_FRAGMENT");
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
+
 }
